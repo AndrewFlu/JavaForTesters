bf5274393a00f2ca696240e981fb600b7d429d8c
AndrewF <andreyshalagin@gmail.com>
2017-12-28 16:11:39 +0300

    Домашнее задание: заполнить многомерный массив через циклы

--- src/test/java/com/javafortesters/chapter009/MainClass.java
+++ src/test/java/com/javafortesters/chapter009/MainClass.java
@@ -1,13 +1,42 @@
 package com.javafortesters.chapter009;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class MainClass {
     public static void main(String[] args) {
+        // создаем экземпляр класса
         UserTests array = new UserTests();
-        int [][] ragged2d = {
-                {},
-                {},
-                {1,2    }
-        };
-        array.print2DIntArray(ragged2d);
+
+        // объявляем многомерный массив
+        int [][] triangleArray = new int[15][];
+
+        // определяем вторую размерность массива
+        for (int i = 0; i<triangleArray.length; i++){
+            triangleArray[i] = new int[i+1];
+        }
+
+        triangleArray[0][0] = 0;
+        triangleArray[1][0] = 0;
+        triangleArray[1][1] = 1;
+        triangleArray[2][0] = 1;
+        int i = 0;
+        // заполняем многомерный массив
+
+
+//        for( int [] outer : triangleArray){
+//            if (outer == null){
+//                System.out.println("Null");
+//                break;
+//            } else{
+//                for (int inner : outer){
+//
+//                    i++;
+//                }
+//            }
+//        }
+
+
+        array.print2DIntArray(triangleArray);
     }
+
 }
