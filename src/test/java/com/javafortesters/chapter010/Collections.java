1052384e852754d02a67bb4e1258e69b378358e1
AndrewF <andreyshalagin@gmail.com>
2018-01-17 10:00:07 +0300

    Chapter 10
    add SimpleArrayExample class

--- src/test/java/com/javafortesters/chapter010/Collections.java
+++ src/test/java/com/javafortesters/chapter010/Collections.java
@@ -1,4 +1,16 @@
 package com.javafortesters.chapter010;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Collections {
+    @Test
+    public void simpleArrayExample(){
+        String[] numbers0123 = {"zero", "one", "two", "three"};
+        for (String number : numbers0123) {
+            System.out.println(number);
+        }
+        Assert.assertEquals("zero", numbers0123[0]);
+        Assert.assertEquals("three", numbers0123[3]);
+    }
 }
