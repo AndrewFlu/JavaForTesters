d98936a0982aa3d74aa010475656bc7dc0fa3a17
AndrewF <andreyshalagin@gmail.com>
2018-01-17 10:07:18 +0300

    Chapter 10
    add SimpleCollectionExample class

--- src/test/java/com/javafortesters/chapter010/Collections.java
+++ src/test/java/com/javafortesters/chapter010/Collections.java
@@ -3,6 +3,9 @@ package com.javafortesters.chapter010;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Collections {
     @Test
     public void simpleArrayExample(){
@@ -13,4 +16,18 @@ public class Collections {
         Assert.assertEquals("zero", numbers0123[0]);
         Assert.assertEquals("three", numbers0123[3]);
     }
+
+    @Test
+    public void simpleCollectionExample(){
+        String [] numbers0123Array = {"zero", "one", "two", "three"};
+        List<String> numbers0123 = Arrays.asList(numbers0123Array);
+        for (String number:numbers0123) {
+            System.out.println(number);
+        }
+        Assert.assertEquals("zero", numbers0123.get(0));
+        Assert.assertEquals("two", numbers0123.get(2));
+        Assert.assertEquals(4, numbers0123.size());
+    }
+
+    
 }
